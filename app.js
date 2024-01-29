4955de7afb7be7ebfa026754c558f3e7aa8b3108
justthatonedude <jothamgao@gmail.com>
2024-01-29 01:05:48 -0500

    3

--- app.js
+++ app.js
@@ -1,20 +1,19 @@
-const URL = "https://opensky-network.org/api/flights/departure?airport="
+
 const DOMSelectors = {
     searchQuery: document.querySelector("#search"),
     form: document.querySelector("#form"),
 }
 
-async function apiCall(URL){
+async function apiCall(){
         const userInput = DOMSelectors.searchQuery.value
-        const unixTime = Date.now();
-        const beginningTime = Date.now() - 60000
-        const response = await fetch(URL + `${userInput}&begin=${beginningTime}&end=${unixTime}`);
-        const result = await response.text();
+        const URL = `https://api.aviationapi.com/v1/weather/metar?=${userInput}`
+        const response = await fetch(URL,{});
+        const result = await response.json();
         console.log(result)
         
 }
 
 DOMSelectors.form.addEventListener("submit", function(event){
     event.preventDefault();
-    apiCall(URL)
+    apiCall()
 })
