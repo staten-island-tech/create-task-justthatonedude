8ebd4c506ce0d6d1408d3f757c9b100ebc4131db
justthatonedude <jothamgao@gmail.com>
2024-01-30 10:27:16 -0500

    4

--- app.js
+++ app.js
@@ -6,8 +6,10 @@ const DOMSelectors = {
 
 async function apiCall(){
         const userInput = DOMSelectors.searchQuery.value
-        const URL = `https://api.aviationapi.com/v1/weather/metar?=${userInput}`
-        const response = await fetch(URL,{});
+        const URL = `https://opensky-network.org/api/states/all?icao24=${userInput}`
+        const response = await fetch(URL,{
+            mode: "cors"
+        });
         const result = await response.json();
         console.log(result)
         
