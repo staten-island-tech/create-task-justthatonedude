2390aaebece98c275ee97f7b786f0b370de11341
justthatonedude <jothamgao@gmail.com>
2024-02-01 10:12:01 -0500

    5

--- app.js
+++ app.js
@@ -1,21 +1,35 @@
-
+let arr = [];
 const DOMSelectors = {
+    container: document.querySelector(".container"),
     searchQuery: document.querySelector("#search"),
     form: document.querySelector("#form"),
 }
 
 async function apiCall(){
         const userInput = DOMSelectors.searchQuery.value
-        const URL = `https://opensky-network.org/api/states/all?icao24=${userInput}`
+        const URL = `https://opensky-network.org/api/states/all?time=1458564121&icao24=${userInput}`
         const response = await fetch(URL,{
             mode: "cors"
         });
         const result = await response.json();
-        console.log(result)
-        
+        arr.push(result)
+        cardInsert(arr)
 }
 
 DOMSelectors.form.addEventListener("submit", function(event){
     event.preventDefault();
     apiCall()
 })
+
+function cardInsert(array){
+    let i = 0
+    let arraylen = array.length
+    for(i = 0; i <= arraylen; i++){
+        if (array != null){
+            DOMSelectors.container.insertAdjacentHTML(
+                "afterbegin",
+                
+            )
+        }
+    }
+}
